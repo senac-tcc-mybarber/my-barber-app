100ea5aae0d407afb52f6d69ed0b8668b273399e
leandrohonorio7 <leandrohonorio7@gmail.com>
2020-02-15 00:18:25 -0300

    Refatoração do código de chamada a API para realidar login. Acrescimo de tipo password no campo senha da tela de login.

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -3,47 +3,35 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { map } from 'rxjs/operators';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { Usuario } from './model/Usuario';
+import { environment } from 'src/environments/environment';
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class RestService {
   private currentUserSubject: BehaviorSubject<Usuario>;
   public currentUser: Observable<Usuario>;
 
-
-  API_KEY = 'password';
   constructor(private http: HttpClient) {
     this.currentUserSubject = new BehaviorSubject<Usuario>(JSON.parse(localStorage.getItem('currentUser')));
     this.currentUser = this.currentUserSubject.asObservable();
-}
-
-  getToken()
-  {
-    const url = 'http://localhost:8080/authenticate';
-    const body = '{"username": "customer", "password": "password"}';
-  //   return this.http.post(url, body, { headers: new HttpHeaders({
-  //     'Access-Control-Allow-Origin':  '*',
-  //     'Access-Control-Allow-Methods': 'POST',
-  //     'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-  //   })
-  // });
-    console.log("inicio");
-  return this.http.post<any>(url, body,
-    { headers: new HttpHeaders
-      ({
-          'Access-Control-Allow-Origin':  '*',
-          'Access-Control-Allow-Methods': 'POST',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-      })
-    }).pipe(map(user => {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('currentUser', JSON.stringify(user));
-                this.currentUserSubject.next(user);
-                console.log(user);
-                return user;
-            }));
-    
-
   }
 
+  getToken(username: string, password: string) {
+    const url = `${environment.urlApi}/authenticate`;
+    const body = '{"username": "' + username + '", "password": "' + password + '"}';
+    return this.http.post<any>(url, body,
+      {
+        headers: new HttpHeaders
+          ({
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+          })
+      }).pipe(map(user => {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        console.log(user);
+        return user;
+      }));
+  }
 }
--- src/app/views/login/login-component/login.component.ts
+++ src/app/views/login/login-component/login.component.ts
@@ -16,20 +16,17 @@ import { first } from 'rxjs/operators';
 export class LoginComponent implements OnInit {
 
   private login: Login = new Login();
-  constructor(private api: RestService) { 
-    
+  constructor(private api: RestService) {
+
   }
 
-  ngOnInit() {
-    // this.api.getToken().subscribe((data)=> {console.log(data); this.article = data['article'];});
-  }
+  ngOnInit() { }
 
-  entrar(){
-    const json: string = "{'username': '" + this.login.username + "', 'password': ''" + this.login.password + "'}";
-    this.api.getToken().pipe(first())
-    .subscribe(
+  entrar() {
+    this.api.getToken(this.login.username, this.login.password).pipe(first())
+      .subscribe(
         data => {
-            console.log("sucesso login component");
+          console.log("sucesso login component");
         },
         error => {
           console.log("erro");
