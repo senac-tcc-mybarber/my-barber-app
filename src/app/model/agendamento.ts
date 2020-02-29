8bf1f72813a44ae83c0c0bdc2793d6b6dec1a2de
Rafael Cabral <rafaelmcabral@gmail.com>
2020-02-29 02:38:44 -0300

    Ajuste do arquivo agendamento.ts que estava acusando exclusão

---
+++ src/app/model/agendamento.ts
@@ -0,0 +1,18 @@
+import {Cliente} from "./cliente";
+import {Profissional} from "./profissional";
+import {Salao} from "./salao";
+import {Servico} from "./Servico";
+
+export class Agendamento {
+  id: number;
+  cliente: Cliente;
+  profissional: Profissional;
+  salao: Salao;
+  servico: Servico;
+  inicioServico: Date;
+  fimServico: Date;
+  checkInCliente: Date;
+  checkInProfissional: Date;
+  checkOutCliente: Date;
+  checkOutProfissional: Date;
+}
