0e0b59ad0ff4463253321551b6582723460a4f8d
fernandoferrandini <fferrandini@clarkemodet.com.br>
2020-03-13 12:12:48 -0300

    adicionada tela para editar cliente

---
+++ src/app/views/cliente/edit-cliente/edit-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cliente',
+  templateUrl: './edit-cliente.component.html',
+  styleUrls: ['./edit-cliente.component.scss']
+})
+export class EditClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
