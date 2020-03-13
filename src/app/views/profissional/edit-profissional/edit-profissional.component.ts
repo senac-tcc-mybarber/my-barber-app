c5d3d68d32227e5227782abaf1d74edeb0826202
fernandoferrandini <fferrandini@clarkemodet.com.br>
2020-03-13 10:02:06 -0300

    call api para editar profissional

---
+++ src/app/views/profissional/edit-profissional/edit-profissional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-profissional',
+  templateUrl: './edit-profissional.component.html',
+  styleUrls: ['./edit-profissional.component.scss']
+})
+export class EditProfissionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
