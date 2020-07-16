90531962771d413bab0b4a91e2a92b94fc1f711b
leandrohonorio7 <leandrohonorio7@gmail.com>
2020-07-16 02:10:54 -0300

    ajuste de layout da home cliente e profissional

--- src/app/views/cliente/home-cliente/home-cliente.component.ts
+++ src/app/views/cliente/home-cliente/home-cliente.component.ts
@@ -1,37 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Cliente } from '../../../model/cliente';
-import { RestService } from '../../../rest.service';
-import { UsuarioService } from '../../../usuario.service'
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Cliente } from "../../../model/cliente";
+import { RestService } from "../../../rest.service";
+import { UsuarioService } from "../../../usuario.service";
 
 @Component({
-  selector: 'app-home-cliente',
-  templateUrl: './home-cliente.component.html',
-  styleUrls: ['./home-cliente.component.scss']
+  selector: "app-home-cliente",
+  templateUrl: "./home-cliente.component.html",
+  styleUrls: ["./home-cliente.component.scss"],
 })
 export class HomeClienteComponent implements OnInit {
-  
   cliente = new Cliente();
 
   constructor(
     private api: RestService,
     private route: ActivatedRoute,
     private router: Router,
-    private usuarioService: UsuarioService) {
+    private usuarioService: UsuarioService
+  ) {}
+
+  public innerWidth: any;
+  public colunms: string[];
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+
+    this.colunms = [
+      "servico",
+      "horario",
+      "local",
+      "status",
+      "atendente",
+      "checkincliente",
+      // "concluirAtendimento",
+    ];
+
+    if(this.innerWidth < 380){
+      this.removeArrayElement("atendente");
+      this.removeArrayElement("status");
+    }
+    else if(this.innerWidth < 450){
+      this.removeArrayElement("status");
     }
 
-  ngOnInit() {
-    this.getCliente()
+    this.getCliente();
+  }
+
+  removeArrayElement(key:string){
+    const index = this.colunms.indexOf(key, 0);
+    if (index > -1) {
+      this.colunms.splice(index, 1);
+    }
   }
 
   getCliente() {
-    const user = this.usuarioService.currentUserValue
-    this.api.getCliente(user.id)
-      .subscribe(data => {
-        this.cliente = data;
-        this.cliente.agendamentos = this.cliente.agendamentos.filter(x => 
-        x.status != 'CONCLUIDO'
-        && x.status != 'CANCELADO' )
-      });
+    const user = this.usuarioService.currentUserValue;
+    this.api.getCliente(user.id).subscribe((data) => {
+      this.cliente = data;
+      this.cliente.agendamentos = this.cliente.agendamentos.filter(
+        (x) => x.status != "CONCLUIDO" && x.status != "CANCELADO"
+      );
+    });
   }
 }
--- src/app/views/profissional/home-profissional/home-profissional.component.ts
+++ src/app/views/profissional/home-profissional/home-profissional.component.ts
@@ -16,8 +16,29 @@ export class HomeProfissionalComponent implements OnInit {
   constructor(private api: RestService, private route: ActivatedRoute, private router: Router, private usuarioService: UsuarioService){
   }
 
+  public innerWidth: any;
+  public colunms: string[];
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+
+    this.colunms = ['cliente', 'servico', 'horario', 'local', 'status', 'checkinprofissional'];
+
+    if(this.innerWidth < 380){
+      this.removeArrayElement("cliente");
+      this.removeArrayElement("status");
+    }
+    else if(this.innerWidth < 450){
+      this.removeArrayElement("status");
+    }
+
     this.getProfissional();
+  }
+
+  removeArrayElement(key:string){
+    const index = this.colunms.indexOf(key, 0);
+    if (index > -1) {
+      this.colunms.splice(index, 1);
+    }
   }
 
   getProfissional() {
@@ -25,6 +46,9 @@ export class HomeProfissionalComponent implements OnInit {
     this.api.getProfissional(user.id)
       .subscribe(data => {
         this.profissional = data;
+        this.profissional.agendamentos = this.profissional.agendamentos.filter(
+          (x) => x.status != "CONCLUIDO" && x.status != "CANCELADO"
+        );
       });
   }
 }
